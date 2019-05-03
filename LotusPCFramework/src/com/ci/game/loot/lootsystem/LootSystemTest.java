2d5b0cd53ba0d8829254db74a799ae4d4e425c99
Jeremiah Larsen <decuadros@gmail.com>
2019-05-02 20:50:51 -0700

    Initial JUnit test creation.  Lots of changes to config and plugins for JUnit tests.

---
+++ LotusPCFramework/src/com/ci/game/loot/lootsystem/LootSystemTest.java
@@ -0,0 +1,17 @@
+package com.ci.game.loot.lootsystem;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LootSystemTest {
+
+	@Test
+	public void calcGoldTest() {
+		LootSystem lootSystem = new LootSystem();
+		lootSystem.lootSystem();
+		
+		fail("Not yet implemented");
+	}
+
+}
